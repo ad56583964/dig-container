a3903c0328dfd5d9348448dc6d9fd2daf5c74a31
emasi <ad56583964@outlook.com>
2024-05-10 02:26:30 +0800

    wip

--- container/run.sh
+++ container/run.sh
@@ -1,1 +1,16 @@
-echo hello
+#!/bin/bash
+
+# 启动容器
+docker-compose up -d
+
+# 等待容器启动
+echo "Waiting for the container to start..."
+sleep 5
+
+# 检查容器状态
+if docker-compose ps | grep -q "Up"; then
+    echo "Container is up, entering shell..."
+    docker-compose exec env bash
+else
+    echo "Container failed to start."
+fi
--- envbox.sh
+++ envbox.sh
@@ -2,7 +2,7 @@
 
 TOPDIR=$(dirname $0)
 
-docker run --it -n envbox \
+docker run -it -n envbox \
 --network host \
 -v ${TOPDIR}/workdir:/home/user/workdir
 -v 
